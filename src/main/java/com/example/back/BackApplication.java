b82b8922a987230743747366ebb9dab99a10b389
thibaut_cherel <thibaut.cherel@adenza.com>
2022-02-15 16:28:09 +0100

    Test curl
    Change-Id: I343224993fcbe46c62dfda4c63911b3ee607b29c

--- src/main/java/com/example/back/BackApplication.java
+++ src/main/java/com/example/back/BackApplication.java
@@ -1,5 +1,9 @@
 package com.example.back;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,27 @@ public class BackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
+		String command = "curl -GET \"localhost:9200/books/_search?pretty\"";
+		ProcessBuilder process = new ProcessBuilder(command); 
+		Process p;
+		try
+		{
+			p = process.start();
+			 BufferedReader reader =  new BufferedReader(new InputStreamReader(p.getInputStream()));
+				StringBuilder builder = new StringBuilder();
+				String line = null;
+				while ( (line = reader.readLine()) != null) {
+						builder.append(line);
+						builder.append(System.getProperty("line.separator"));
+				}
+				String result = builder.toString();
+				System.out.print(result);
+	
+		}
+		catch (IOException e)
+		{   System.out.print("error");
+			e.printStackTrace();
+		}
 	}
 
 }
