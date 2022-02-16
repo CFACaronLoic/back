5520ab549069c4f1c3167c5c85ef15ab7ff9f32a
Eric <e.khamvongsa@cfa-insta.fr>
2022-02-16 11:10:08 +0100

    CrossOrigin autorisé

--- src/main/java/com/example/back/BookController.java
+++ src/main/java/com/example/back/BookController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 
 @RestController
 class BookController {
